e5c59346cf3c2937405859b5ab78143325635473
ariannagreco3393 <a.greco3393@gmail.com>
2022-05-20 10:35:17 +0200

    fix bug

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 Creare una variabile con un paragrafo di testo a vostra scelta.
 Stampare a schermo il paragrafo e la sua lunghezza.
 Una parola da censurare viene passata dall'utente tramite parametro GET.
-Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) 
+tutte le occorrenze della parola da censurare.
  -->
 
 <?php
@@ -13,9 +14,6 @@ Quasi voluptatem natus illo praesentium quae, enim qui at facere. Voluptate, rei
 //lunghezza del paragrafo
 $length = strlen($paragraph);
 
-//stampa a schermo
-
-
 
 //parola passata con GET
 $word = $_GET["word"];
@@ -23,8 +21,6 @@ $word = $_GET["word"];
 $new_paragraph = str_replace($word, "***", $paragraph);
 
 $new_length = strlen($new_paragraph);
-
-
 
 ?>
 
@@ -39,7 +35,8 @@ $new_length = strlen($new_paragraph);
 </head>
 
 <body>
-    <div class="container" style="max-width: 1200px; margin: 0 auto;">
+    <div class="container" style="max-width: 1000px; margin: 0 auto;">
+        <!-- stampa a schermo -->
         <p style="color:blue ;"><?php echo $paragraph; ?></p>
         <p>Lunghezza: <?php echo $length; ?></p>
         <p style="color:red ;"><?php echo $new_paragraph; ?></p>
